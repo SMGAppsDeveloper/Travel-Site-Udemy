2d8ac1400a3e51aedf58d1214a903dede37751a0
SMGAppDeveloper <smg.app.developer@gmail.com>
2018-06-09 11:41:57 -0700

    Gulp and more of its functinality

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', function(){
 	console.log("Good. you have created gulp");
@@ -8,7 +12,9 @@ gulp.task('html', function(){
 	console.log("Image something usefull being done to your HTML hear");
 });
 gulp.task('styles', function(){
-	console.log("Image Sass or PostCss tasks running here. ");
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
 });
 gulp.task('watch', function(){
 	watch('./app/index.html', function(){
