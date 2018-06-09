f8d76ad65d8109b8b22ae5b3bdfb345914f62679
SMGAppDeveloper <smg.app.developer@gmail.com>
2018-06-09 08:50:32 -0700

    install NPM pakages

---
+++ node-experiment/test.js
@@ -0,0 +1,15 @@
+var fileSystem = require('fs');
+var https = require('https');
+
+fileSystem.writeFile(__dirname + "/index.html", "<h1>HTML is great</h1>", function(error){
+	if(error){
+		return console.log(error);
+	}
+	else{
+		return console.log("Congrats!");
+	}
+});
+var myPhonePaht = 'https://raw.githubusercontent.com/LearnWebCode/welcome-to-git/master/images/dog.jpg'
+https.get(myPhonePaht, function(response){
+	response.pipe(fileSystem.createWriteStream(__dirname + "/myDog.jpg"));
+});
